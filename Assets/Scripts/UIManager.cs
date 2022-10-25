9b700cfc2cf41f40de7089420071f9edf29ad43e
Xingjian Ma <xima2572@uni.sydney.edu.au>
2022-10-25 16:12:20 +1100

    a part of UI appearance

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    private Button quitButton;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +29,24 @@ public class UIManager : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
 
-        //SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex == 0)
+        {
+            if (GameObject.FindGameObjectWithTag("QuitButton"))
+            {
+                quitButton = GameObject.FindGameObjectWithTag("QuitButton").GetComponent<Button>();
+
+                quitButton.onClick.AddListener(QuitGame);
+            }
+        }
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 }
